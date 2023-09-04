d44a40b3208921806bf1161bb1162ce3741ab9aa
Robbie Blaine <robbieblaine.rb@gmail.com>
2023-09-04 18:14:47 +0200

    Runtime `NEXT_PUBLIC` environment variables
    * Add `/api/config/client-env` which returns a JSON with all env variables
      that are prefixed with `NEXT_PUBLIC_`
    * This then allows for runtime resolution of `NEXT_PUBLIC_*` env variables
    * Specifically `NEXT_PUBLIC_API_BASE_URL`

--- src/web/youth/src/api/services/user.ts
+++ src/web/youth/src/api/services/user.ts
@@ -2,6 +2,6 @@ import ApiClient from "~/lib/axiosClient";
 import type { User, UserProfileRequest } from "../models/user";
 
 export const patchUser = async (model: UserProfileRequest): Promise<User> => {
-  const { data } = await ApiClient.patch<User>("/user", model);
+  const { data } = await (await ApiClient).patch<User>("/user", model);
   return data;
 };
