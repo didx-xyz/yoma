e5ca43a102b53c31efaab5416ab2b707f728060d
Adrian Wium <adrian.wium@outlook.com>
2023-09-26 08:35:36 +0200

    Fixed using cleanup issues

--- src/api/src/infrastructure/Yoma.Core.Infrastructure.AriesCloud/Startup.cs
+++ src/api/src/infrastructure/Yoma.Core.Infrastructure.AriesCloud/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Yoma.Core.Domain.SSI.Interfaces.Provider;
 using Yoma.Core.Infrastructure.AriesCloud.Client;
 using Yoma.Core.Infrastructure.AriesCloud.Context;
 
