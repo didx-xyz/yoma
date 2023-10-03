680aec108d4fe7de41086d2a75f8659b6cebed60
Jason Dicker <dickerjason@gmail.com>
2023-10-03 11:35:53 +0200

    credential model

--- src/web/youth/src/api/services/credentials.ts
+++ src/web/youth/src/api/services/credentials.ts
@@ -1,7 +1,11 @@
 import ApiClient from "~/lib/axiosClient";
 import type { GetServerSidePropsContext } from "next";
 import ApiServer from "~/lib/axiosServer";
-import type { SSISchema, SSISchemaEntity } from "../models/credential";
+import type {
+  SSISchema,
+  SSISchemaEntity,
+  SSISchemaRequest,
+} from "../models/credential";
 
 export const getSchemas = async (
   context?: GetServerSidePropsContext,
@@ -22,6 +26,17 @@ export const getSchemaEntities = async (
 export const createSchema = async (
   model: SSISchemaRequest,
 ): Promise<SSISchema> => {
-  const { data } = await (await ApiClient).post<SSISchema>("/schema", model);
+  const { data } = await (
+    await ApiClient
+  ).post<SSISchema>("/ssi/schema", model);
+  return data;
+};
+
+export const getSchemaByName = async (
+  name: string,
+  context?: GetServerSidePropsContext,
+): Promise<SSISchema> => {
+  const instance = context ? ApiServer(context) : await ApiClient;
+  const { data } = await instance.get<SSISchema>(`/ssi/schema/${name}`);
   return data;
 };
