a7cb1da6ca50479a435271257e896d5edebd2efd
Jason Dicker <dickerjason@gmail.com>
2024-01-29 13:31:17 +0200

    e2e fixes (timeouts)

--- cypress/e2e/opportunities.cy.ts
+++ cypress/e2e/opportunities.cy.ts
@@ -32,7 +32,7 @@ describe(`Opportunities`, function () {
           cy.stub(win.console, "error").as("consoleError");
         },
       });
-      cy.wait(10000);
+      cy.wait(20000);
 
       //* click on the first organisation link on the user menu
       cy.get(`button[id="btnUserMenu"]`).should("exist").click();
