f7c6b8c2994cd8bc83a6c6d26874b6cef6e9224f
wdbasson <wlmbasson@gmail.com>
2024-01-31 09:44:49 +0200

    Initialisation flag per script

--- src/api/cicd/scripts/postgressql-init/post.sh
+++ src/api/cicd/scripts/postgressql-init/post.sh
@@ -9,22 +9,31 @@ echo "Test login to PostgreSQL" >> /dev/stdout
 # You can use 'psql' to execute SQL commands in PostgreSQL
 psql -c "SELECT version();" >> /dev/stdout
 
-# Check if the initialization_flag table exists and the flag value is 'executed'
-result=$(psql -t -c "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'script_execution' AND table_schema = 'public');")
-table_exists=$(echo "$result" | tr -d ' ')
+# Create the script_execution table if it does not exist
+psql -c "CREATE TABLE IF NOT EXISTS script_execution (script_name VARCHAR PRIMARY KEY, executed BOOLEAN NOT NULL DEFAULT FALSE);" >> /dev/stdout
 
-if [ "$table_exists" != "f" ]; then
-  for file in "${sql_files[@]}"; do
-    echo "########## running $file ##########" >> /dev/stdout
-    # Execute the SQL file using 'psql'
-    psql -a -f "$file" >> /dev/stdout
-  done
-  # Create the initialization_flag table if it does not exist
-  psql -c "CREATE TABLE IF NOT EXISTS script_execution (script_name VARCHAR PRIMARY KEY, executed BOOLEAN NOT NULL DEFAULT FALSE);" >> /dev/stdout
-else
-  echo "########## DB already initialized ##########" >> /dev/stdout
-  exit 0
-fi
+# Process each SQL file
+for file in "${sql_files[@]}"; do
+    # Extract filename without path for logging
+    filename=$(basename "$file")
+
+    echo "Checking execution status for $filename" >> /dev/stdout
+    # Check if this script has been executed
+    result=$(psql -t -c "SELECT executed FROM script_execution WHERE script_name = '$filename';")
+    script_executed=$(echo "$result" | tr -d '[:space:]')
+
+    if [ "$script_executed" != "t" ]; then
+        echo "########## running $filename ##########" >> /dev/stdout
+        # Execute the SQL file using 'psql'
+        psql -f "$file" >> /dev/stdout
+
+        echo "Marking $filename as executed" >> /dev/stdout
+        # Mark this script as executed in the script_execution table
+        psql -c "INSERT INTO script_execution (script_name, executed) VALUES ('$filename', TRUE) ON CONFLICT (script_name) DO UPDATE SET executed = TRUE;" >> /dev/stdout
+    else
+        echo "########## $filename already executed ##########" >> /dev/stdout
+    fi
+done
 
 # Exit to signal that the initialization is complete
 echo "Initialization complete." >> /dev/stdout
