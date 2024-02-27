6424443532943294f96c3d38f98bd06b2035dac0
adrianwium <82496337+adrianwium@users.noreply.github.com>
2024-02-27 15:54:30 +0200

    Linting

--- src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OpporunityCompletion.cs
+++
@@ -1,9 +0,0 @@
-namespace Yoma.Core.Domain.Analytics.Models
-{
-    public class OpporunityCompletion
-    {
-        public int AverageTimeInDays { get; set; }
-
-        public decimal? Percentage { get; set; } //v3.1
-    }
-}
