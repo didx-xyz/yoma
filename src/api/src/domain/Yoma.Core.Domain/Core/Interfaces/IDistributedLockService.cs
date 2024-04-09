eb5fc4cf73106efddd93972e9c8dd744b895e72d
adrianwium <82496337+adrianwium@users.noreply.github.com>
2024-04-09 15:12:46 +0200

    Adding redis and used it to implement a DistributedLockService for hangfire jobs

---
+++ src/api/src/domain/Yoma.Core.Domain/Core/Interfaces/IDistributedLockService.cs
@@ -0,0 +1,8 @@
+﻿namespace Yoma.Core.Domain.Core.Interfaces
+{
+  public interface IDistributedLockService
+  {
+    Task ReleaseLockAsync(string key);
+    Task<bool> TryAcquireLockAsync(string key, TimeSpan lockDuration);
+  }
+}
