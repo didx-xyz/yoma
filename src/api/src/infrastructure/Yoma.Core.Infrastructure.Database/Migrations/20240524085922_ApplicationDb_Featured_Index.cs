028c0d02757025993bd93b3d1fdcdd1eb3aa295c
adrianwium <82496337+adrianwium@users.noreply.github.com>
2024-05-24 11:03:22 +0200

    Linting

--- src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20240524085922_ApplicationDb_Featured_Index.cs
+++ src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20240524085922_ApplicationDb_Featured_Index.cs
@@ -1,40 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Yoma.Core.Infrastructure.Database.Migrations
 {
+  /// <inheritdoc />
+  public partial class ApplicationDb_Featured_Index : Migration
+  {
     /// <inheritdoc />
-    public partial class ApplicationDb_Featured_Index : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
-                schema: "Opportunity",
-                table: "Opportunity");
+      migrationBuilder.DropIndex(
+          name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
+          schema: "Opportunity",
+          table: "Opportunity");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
-                schema: "Opportunity",
-                table: "Opportunity",
-                columns: new[] { "TypeId", "OrganizationId", "ZltoReward", "DifficultyId", "CommitmentIntervalId", "CommitmentIntervalCount", "StatusId", "Keywords", "DateStart", "DateEnd", "CredentialIssuanceEnabled", "Featured", "DateCreated", "CreatedByUserId", "DateModified", "ModifiedByUserId" });
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
-                schema: "Opportunity",
-                table: "Opportunity");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
-                schema: "Opportunity",
-                table: "Opportunity",
-                columns: new[] { "TypeId", "OrganizationId", "ZltoReward", "DifficultyId", "CommitmentIntervalId", "CommitmentIntervalCount", "StatusId", "Keywords", "DateStart", "DateEnd", "CredentialIssuanceEnabled", "DateCreated", "CreatedByUserId", "DateModified", "ModifiedByUserId" });
-        }
+      migrationBuilder.CreateIndex(
+          name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
+          schema: "Opportunity",
+          table: "Opportunity",
+          columns: new[] { "TypeId", "OrganizationId", "ZltoReward", "DifficultyId", "CommitmentIntervalId", "CommitmentIntervalCount", "StatusId", "Keywords", "DateStart", "DateEnd", "CredentialIssuanceEnabled", "Featured", "DateCreated", "CreatedByUserId", "DateModified", "ModifiedByUserId" });
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropIndex(
+          name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
+          schema: "Opportunity",
+          table: "Opportunity");
+
+      migrationBuilder.CreateIndex(
+          name: "IX_Opportunity_TypeId_OrganizationId_ZltoReward_DifficultyId_C~",
+          schema: "Opportunity",
+          table: "Opportunity",
+          columns: new[] { "TypeId", "OrganizationId", "ZltoReward", "DifficultyId", "CommitmentIntervalId", "CommitmentIntervalCount", "StatusId", "Keywords", "DateStart", "DateEnd", "CredentialIssuanceEnabled", "DateCreated", "CreatedByUserId", "DateModified", "ModifiedByUserId" });
+    }
+  }
 }
