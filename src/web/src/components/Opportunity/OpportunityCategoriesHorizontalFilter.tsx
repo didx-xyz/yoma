d046cdba2c48284ac6ae2ec8e18e951e8b031cac
Jason Dicker <dickerjason@gmail.com>
2024-06-24 07:40:15 +0200

    use state for totalSlides

--- src/web/src/components/Opportunity/OpportunityCategoriesHorizontalFilter.tsx
+++ src/web/src/components/Opportunity/OpportunityCategoriesHorizontalFilter.tsx
@@ -23,7 +23,7 @@ const OpportunityCategoriesHorizontalFilter: React.FC<{
   const screenWidth = useAtomValue(screenWidthAtom);
   const [visibleSlides, setVisibleSlides] = useState(2);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const totalSlides = lookups_categories.length;
+  const [totalSlides] = useState(lookups_categories?.length ?? 0);
 
   useEffect(() => {
     if (screenWidth < 480) {
@@ -82,9 +82,7 @@ const OpportunityCategoriesHorizontalFilter: React.FC<{
           trayProps={{
             // center align the slides when there's less than the visible slides available
             className: `${
-              lookups_categories.length < visibleSlides
-                ? "flex justify-center"
-                : ""
+              totalSlides < visibleSlides ? "flex justify-center" : ""
             }`,
           }}
         >
@@ -114,7 +112,7 @@ const OpportunityCategoriesHorizontalFilter: React.FC<{
             <path
               fill="currentColor"
               stroke="currentColor"
-              stroke-width="45"
+              strokeWidth="45"
               d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
             />
           </svg>
@@ -137,7 +135,7 @@ const OpportunityCategoriesHorizontalFilter: React.FC<{
             <path
               fill="currentColor"
               stroke="currentColor"
-              stroke-width="45"
+              strokeWidth="45"
               d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
             />
           </svg>
