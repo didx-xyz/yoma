1fb2b8c46d9031d471e13765262ce59746548a43
adrianwium <82496337+adrianwium@users.noreply.github.com>
2024-07-22 12:35:30 +0200

    Linting

--- src/api/src/domain/Yoma.Core.Domain/MyOpportunity/Models/MyOpportunitySearchResultsSummary.cs
+++ src/api/src/domain/Yoma.Core.Domain/MyOpportunity/Models/MyOpportunitySearchResultsSummary.cs
@@ -2,7 +2,7 @@ using Yoma.Core.Domain.Core.Models;
 
 namespace Yoma.Core.Domain.MyOpportunity.Models
 {
-  public class MyOpportunitySearchResultsSummary 
+  public class MyOpportunitySearchResultsSummary
   {
     public TimeIntervalSummary? MyOpportunities { get; set; }
   }
