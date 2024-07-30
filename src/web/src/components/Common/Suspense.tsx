42388230a6415c4ab0e75d0c3e41da520b8083da
Jason Dicker <dickerjason@gmail.com>
2024-07-30 16:20:51 +0200

    build error

--- src/web/src/components/Common/Suspense.tsx
+++ src/web/src/components/Common/Suspense.tsx
@@ -2,18 +2,12 @@ import { ApiErrors } from "../Status/ApiErrors";
 import { LoadingSkeleton } from "../Status/LoadingSkeleton";
 
 interface SuspenseProps {
-  //isReady: boolean;
   isLoading: boolean;
   error?: any;
   children: React.ReactNode;
 }
 
-const Suspense: React.FC<SuspenseProps> = ({
-  //isReady,
-  isLoading,
-  error,
-  children,
-}) => {
+const Suspense: React.FC<SuspenseProps> = ({ isLoading, error, children }) => {
   if (isLoading) {
     return <LoadingSkeleton />;
   }
