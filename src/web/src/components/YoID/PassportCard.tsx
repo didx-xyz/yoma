f8ec115ff3f45d911885ddee4e150b29967e8fc3
Jason Dicker <dickerjason@gmail.com>
2024-07-31 10:50:10 +0200

    yoid: passport card stamps images and layout

--- src/web/src/components/YoID/PassportCard.tsx
+++ src/web/src/components/YoID/PassportCard.tsx
@@ -1,20 +1,45 @@
+import stamps from "public/images/stamps-vertical.svg";
+import Image from "next/image";
+
 export const PassportCard: React.FC<{
   data: { schemaType: string; totalCount: number | null }[];
 }> = ({ data }) => {
   return (
-    <div className="flex flex-grow flex-col justify-center gap-2 text-xs">
-      {data?.map((item) => (
-        <div key={item.schemaType} className="flex flex-row flex-nowrap gap-2">
-          <span className="flex flex-grow items-center whitespace-nowrap">
-            {item.schemaType === "YoID" && "💳"}
-            {item.schemaType === "Opportunity" && "🏆"}
-            <span className="ml-2">{item.schemaType}</span>
-          </span>
-          <span className="badge badge-xs bg-gray">
-            {item.totalCount ?? "?"}
-          </span>
+    <div className="flex flex-grow flex-col justify-center">
+      <Image src={stamps} alt="Stamps" className="absolute" />
+      <div className="flex flex-grow items-center justify-end">
+        <div className="flex flex-col gap-1 text-xs">
+          {data?.map((item) => (
+            <div
+              key={item.schemaType}
+              className="flex flex-row flex-nowrap gap-2"
+            >
+              <span className="flex flex-grow items-center whitespace-nowrap">
+                {item.schemaType === "YoID" && (
+                  <span>
+                    💳<span className="ml-2">Identity</span>
+                  </span>
+                )}
+                {item.schemaType === "Opportunity" && (
+                  <span>
+                    🏆
+                    <span className="ml-2">Opportunities</span>
+                  </span>
+                )}
+                {item.schemaType !== "YoID" &&
+                  item.schemaType !== "Opportunity" && (
+                    <span>
+                      ❔<span className="ml-2">{item.schemaType}</span>
+                    </span>
+                  )}
+              </span>
+              <span className="badge badge-xs ml-4 min-w-[40px] bg-gray px-2">
+                {item.totalCount ?? "?"}
+              </span>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
