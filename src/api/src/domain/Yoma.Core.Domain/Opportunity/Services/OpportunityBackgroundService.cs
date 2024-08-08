0edaa72039f2147a681e24014239e7bb135408b0
adrianwium <82496337+adrianwium@users.noreply.github.com>
2024-08-08 09:56:16 +0200

    [YOMA-440, YOMA-441, YOMA-442] Youth email opportunity published filtering by country (#996)
    * [YOMA-421] Added opporunity summary to 'my' opporunity models
    
    * Filter opporunity publiched email by country
    
    * Linting

--- src/api/src/domain/Yoma.Core.Domain/Opportunity/Services/OpportunityBackgroundService.cs
+++ src/api/src/domain/Yoma.Core.Domain/Opportunity/Services/OpportunityBackgroundService.cs
@@ -14,6 +14,7 @@ using Yoma.Core.Domain.Entity;
 using Yoma.Core.Domain.Entity.Interfaces;
 using Yoma.Core.Domain.Entity.Interfaces.Lookups;
 using Yoma.Core.Domain.Entity.Models;
+using Yoma.Core.Domain.Lookups.Interfaces;
 using Yoma.Core.Domain.Opportunity.Events;
 using Yoma.Core.Domain.Opportunity.Interfaces;
 using Yoma.Core.Domain.Opportunity.Interfaces.Lookups;
@@ -30,6 +31,7 @@ namespace Yoma.Core.Domain.Opportunity.Services
     private readonly IOrganizationStatusService _organizationStatusService;
     private readonly IEmailProviderClient _emailProviderClient;
     private readonly IUserService _userService;
+    private readonly ICountryService _countryService;
     private readonly IEmailURLFactory _emailURLFactory;
     private readonly IEmailPreferenceFilterService _emailPreferenceFilterService;
     private readonly IRepositoryBatchedValueContainsWithNavigation<Models.Opportunity> _opportunityRepository;
@@ -47,6 +49,7 @@ namespace Yoma.Core.Domain.Opportunity.Services
         IOrganizationStatusService organizationStatusService,
         IEmailProviderClientFactory emailProviderClientFactory,
         IUserService userService,
+        ICountryService countryService,
         IEmailURLFactory emailURLFactory,
         IEmailPreferenceFilterService emailPreferenceFilterService,
         IRepositoryBatchedValueContainsWithNavigation<Models.Opportunity> opportunityRepository,
@@ -60,6 +63,7 @@ namespace Yoma.Core.Domain.Opportunity.Services
       _organizationStatusService = organizationStatusService;
       _emailProviderClient = emailProviderClientFactory.CreateClient();
       _userService = userService;
+      _countryService = countryService;
       _emailURLFactory = emailURLFactory;
       _emailPreferenceFilterService = emailPreferenceFilterService;
       _opportunityRepository = opportunityRepository;
@@ -97,7 +101,7 @@ namespace Yoma.Core.Domain.Opportunity.Services
           var statusActiveId = _opportunityStatusService.GetByName(Status.Active.ToString()).Id;
           var organizationStatusActiveId = _organizationStatusService.GetByName(OrganizationStatus.Active.ToString()).Id;
 
-          var items = _opportunityRepository.Query().Where(o => o.OrganizationStatusId == organizationStatusActiveId
+          var items = _opportunityRepository.Query(true).Where(o => o.OrganizationStatusId == organizationStatusActiveId //include children / countries
              && o.StatusId == statusActiveId && o.DateStart >= datetimeFrom && o.DateStart <= datetimeTo)
             .OrderBy(o => o.DateStart).ThenBy(o => o.Title).ThenBy(o => o.Id).ToList();
           if (items.Count == 0) return;
@@ -311,31 +315,19 @@ namespace Yoma.Core.Domain.Opportunity.Services
     private async Task SendEmailPublished(List<Models.Opportunity> items, DateTimeOffset executeUntil)
     {
       var emailType = EmailType.Opportunity_Published;
+      var countryWorldWideId = _countryService.GetByCodeAplha2(Domain.Core.Country.Worldwide.ToDescription()).Id;
 
       try
       {
         var processedUserIds = new HashSet<Guid>();
-        var data = new EmailOpportunityAnnounced { Opportunities = [] };
-
-        foreach (var item in items)
-        {
-          data.Opportunities.Add(new EmailOpportunityAnnouncedItem
-          {
-            Title = item.Title,
-            DateStart = item.DateStart,
-            DateEnd = item.DateEnd,
-            URL = _emailURLFactory.OpportunityAnnouncedItemURL(emailType, item.Id, item.OrganizationId),
-            ZltoReward = item.ZltoReward,
-            YomaReward = item.YomaReward
-          });
-        }
-
         int pageNumber = 1;
         int pageSize = 1000;
         while (executeUntil > DateTimeOffset.UtcNow)
         {
+          var recipientDataGroups = new List<(List<EmailRecipient> Recipients, EmailOpportunityAnnounced Data)>();
+
           var searchResult = _userService.Search(
-              new UserSearchFilter //implicitly includes only users with an confirmed email
+              new UserSearchFilter // implicitly includes only users with a confirmed email
               {
                 YoIDOnboarded = true,
                 PageNumber = pageNumber,
@@ -344,20 +336,77 @@ namespace Yoma.Core.Domain.Opportunity.Services
           if (searchResult.Items.Count == 0) break;
 
           var users = searchResult.Items
-            .Where(o => !processedUserIds.Contains(o.Id))
-            .ToList();
+              .Where(o => !processedUserIds.Contains(o.Id))
+              .ToList();
           users.ForEach(o => processedUserIds.Add(o.Id));
+          if (users.Count == 0) break;
 
-          var recipients = users.
-            Select(u => new EmailRecipient
+          // group users by country, defaulting to worldwide if not set
+          var usersByCountry = users
+              .GroupBy(u => u.CountryId ?? countryWorldWideId)
+              .ToDictionary(group => group.Key, group => group.ToList());
+
+          foreach (var userGroup in usersByCountry)
+          {
+            var countryId = userGroup.Key;
+
+            // filter opportunities based on the user's country or worldwide, defaulting to worldwide if no countries are set
+            var countryOpportunities = items
+                .Where(opportunity => opportunity.Countries == null ||
+                                      opportunity.Countries.Any(c => c.Id == countryId) ||
+                                      opportunity.Countries.Any(c => c.Id == countryWorldWideId))
+                .ToList();
+            if (countryOpportunities.Count == 0) continue;
+
+            var data = new EmailOpportunityAnnounced
             {
-              Email = u.Email,
-              DisplayName = u.DisplayName
-            }).ToList();
+              Opportunities = countryOpportunities.Select(item => new EmailOpportunityAnnouncedItem
+              {
+                Id = item.Id,
+                Title = item.Title,
+                DateStart = item.DateStart,
+                DateEnd = item.DateEnd,
+                URL = _emailURLFactory.OpportunityAnnouncedItemURL(emailType, item.Id, item.OrganizationId),
+                ZltoReward = item.ZltoReward,
+                YomaReward = item.YomaReward
+              }).ToList()
+            };
 
-          recipients = _emailPreferenceFilterService.FilterRecipients(emailType, recipients);
-          if (recipients != null && recipients.Count > 0)
-            await _emailProviderClient.Send(emailType, recipients, data);
+            // check if an existing group with the same opportunity already exists
+            var existingGroup = recipientDataGroups.FirstOrDefault(group =>
+                new HashSet<Guid>(group.Data.Opportunities.Select(o => o.Id)).SetEquals(new HashSet<Guid>(countryOpportunities.Select(o => o.Id))));
+
+            if (existingGroup.Equals(default((List<EmailRecipient> Recipients, EmailOpportunityAnnounced Data))))
+            {
+              // create a new recipient list
+              var recipients = userGroup.Value.Select(u => new EmailRecipient
+              {
+                Email = u.Email,
+                DisplayName = u.DisplayName
+              }).ToList();
+
+              recipientDataGroups.Add((recipients, data));
+            }
+            else
+            {
+              // add recipients to the existing group
+              existingGroup.Recipients.AddRange(userGroup.Value.Select(u => new EmailRecipient
+              {
+                Email = u.Email,
+                DisplayName = u.DisplayName
+              }).ToList());
+            }
+          }
+
+          // filter recipients by email preferences before proceeding to the next page
+          recipientDataGroups = recipientDataGroups
+              .Select(group => (Recipients: _emailPreferenceFilterService.FilterRecipients(emailType, group.Recipients) ?? [], group.Data))
+              .Where(group => group.Recipients.Count > 0)
+              .ToList();
+
+          // send emails in one go for the paged results
+          if (recipientDataGroups.Count > 0)
+            await _emailProviderClient.Send(emailType, recipientDataGroups);
 
           pageNumber++;
 
