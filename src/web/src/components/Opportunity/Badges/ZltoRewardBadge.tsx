9b8ce982649dfc96752de68666dd64cff96770a3
Jason Dicker <110084728+jasondicker@users.noreply.github.com>
2024-09-18 14:16:02 +0200

    Feature/opportunity reward balance (#1104)
    * opportunity: zlto badge (show depleted for 0 balance)

---
+++ src/web/src/components/Opportunity/Badges/ZltoRewardBadge.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import iconZlto from "public/images/icon-zlto.svg";
+import React from "react";
+import type { OpportunityInfo } from "~/api/models/opportunity";
+
+interface ZltoRewardBadgeProps {
+  opportunity: OpportunityInfo;
+}
+
+const ZltoRewardBadge: React.FC<ZltoRewardBadgeProps> = ({ opportunity }) => {
+  if (opportunity?.zltoReward == null) {
+    return null;
+  }
+
+  return (
+    <>
+      {opportunity.zltoReward === 0 && (
+        <div className="badge bg-orange-light text-orange">
+          <Image
+            src={iconZlto}
+            alt="Icon Zlto"
+            width={16}
+            height={16}
+            sizes="100vw"
+            priority={true}
+            style={{ width: "16px", height: "16px" }}
+          />
+          <span className="ml-1">Depleted</span>
+        </div>
+      )}
+      {opportunity.zltoReward > 0 && (
+        <div className="badge bg-orange-light text-orange">
+          <Image
+            src={iconZlto}
+            alt="Icon Zlto"
+            width={16}
+            height={16}
+            sizes="100vw"
+            priority={true}
+            style={{ width: "16px", height: "16px" }}
+          />
+          <span className="ml-1">{opportunity.zltoReward}</span>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ZltoRewardBadge;
