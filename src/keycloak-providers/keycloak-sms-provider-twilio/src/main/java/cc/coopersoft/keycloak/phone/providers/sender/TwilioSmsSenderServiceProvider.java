5f9c0be825f9678139d48c7460900d5121382422
Jason Dicker <dickerjason@gmail.com>
2024-10-04 09:32:07 +0200

    custom keycloak provider for phone

--- src/keycloak-providers/keycloak-sms-provider-twilio/src/main/java/cc/coopersoft/keycloak/phone/providers/sender/TwilioSmsSenderServiceProvider.java
+++ src/keycloak-providers/keycloak-sms-provider-twilio/src/main/java/cc/coopersoft/keycloak/phone/providers/sender/TwilioSmsSenderServiceProvider.java
@@ -22,17 +22,26 @@ public class TwilioSmsSenderServiceProvider extends FullSmsSenderAbstractService
 
     @Override
     public void sendMessage(String phoneNumber, String message) throws MessageSendException {
-
-        Message msg = Message.creator(
-                new PhoneNumber(phoneNumber),
-                new PhoneNumber(twilioPhoneNumber),
-                message).create();
-
-        if (msg.getStatus() == Message.Status.FAILED) {
-            logger.error("message send failed!");
-            throw new MessageSendException(msg.getStatus().ordinal(),
-                    String.valueOf(msg.getErrorCode()),
-                    msg.getErrorMessage());
+        try {
+            Message msg = Message.creator(
+                    new PhoneNumber(phoneNumber),
+                    new PhoneNumber(twilioPhoneNumber),
+                    message).create();
+            if (msg.getStatus() == Message.Status.FAILED) {
+                logger.error("Message send failed!");
+                // Construct an error message
+                String errorMsg = String.format("Message send failed with status: %s, error code: %s, error message: %s",
+                        msg.getStatus(),
+                        msg.getErrorCode(),
+                        msg.getErrorMessage());
+                // Throw the exception with the error message and no cause
+                throw new MessageSendException(errorMsg, null);
+            }
+        } catch (Exception e) {
+            // If an exception occurs, wrap it in MessageSendException
+            String errorMsg = "Failed to send message due to an exception.";
+            logger.error(errorMsg, e);
+            throw new MessageSendException(errorMsg, e);
         }
     }
 
