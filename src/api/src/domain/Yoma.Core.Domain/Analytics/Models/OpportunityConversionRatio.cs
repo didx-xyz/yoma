77e52a18dc0f244908a450eec1f7734d18b9d200
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-01-30 13:56:39 +0200

    Update conversion ratios (#1216)

--- src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OpportunityConversionRatio.cs
+++ src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OpportunityConversionRatio.cs
@@ -2,12 +2,14 @@ namespace Yoma.Core.Domain.Analytics.Models
 {
   public class OpportunityConversionRatio
   {
-    public string Legend { get; set; }
+    public int ViewedCount { get; set; }
+
+    public int NavigatedExternalLinkCount { get; set; }
 
     public int CompletedCount { get; set; }
 
-    public int ViewedCount { get; set; }
+    public decimal ViewedToNavigatedExternalLinkPercentage { get; set; }
 
-    public decimal Percentage { get; set; }
+    public decimal NavigatedExternalLinkToCompletedPercentage { get; set; }
   }
 }
