8cbdbc6ef7304cc963be38bf1eebaa11bc7af7f9
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-02-06 08:50:45 +0200

    SSO analytics changes to support all, one, or multiple organizations (super admin role) (#1223)
    * SSO analytics changes to support all, one, or multiple organizations (super admin role)
    
    * Linting

---
+++ src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OrganizationSSOInfo.cs
@@ -0,0 +1,30 @@
+using Newtonsoft.Json;
+using Yoma.Core.Domain.BlobProvider;
+
+namespace Yoma.Core.Domain.Analytics.Models
+{
+  public class OrganizationSSOInfo
+  {
+    public string Name { get; set; }
+
+    public Guid? LogoId { get; set; }
+
+    [JsonIgnore]
+    public StorageType? LogoStorageType { get; set; }
+
+    [JsonIgnore]
+    public string? LogoKey { get; set; }
+
+    public string? LogoURL { get; set; }
+
+    /// <summary>
+    /// Outbound SSO allowing logins on third-party systems using Yoma credentials
+    /// </summary>
+    public OrganizationSSO Outbound { get; set; }
+
+    /// <summary>
+    /// Inbound SSO allowing logins on Yoma's site using third-party credentials
+    /// </summary>
+    public OrganizationSSO Inbound { get; set; }
+  }
+}
--- src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OrganizationSearchFilterSSO.cs
+++ src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OrganizationSearchFilterSSO.cs
@@ -1,11 +1,19 @@
+using Yoma.Core.Domain.Core.Models;
+
 namespace Yoma.Core.Domain.Analytics.Models
 {
-  public class OrganizationSearchFilterSSO
+  public class OrganizationSearchFilterSSO : PaginationFilter
   {
-    public Guid Organization { get; set; }
+    public List<Guid>? Organizations { get; set; }
 
     public DateTimeOffset? StartDate { get; set; }
 
     public DateTimeOffset? EndDate { get; set; }
+
+    public void SanitizeCollections()
+    {
+      Organizations = Organizations?.Distinct().ToList();
+      if (Organizations?.Count == 0) Organizations = null;
+    }
   }
 }
