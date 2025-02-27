ad4e5170e8fbdd5a1e3e2526618c5d7925c6aa2c
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-02-27 13:07:44 +0200

    Include counts from navigated external link tracking

--- src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OpportunityConversionRatio.cs
+++ src/api/src/domain/Yoma.Core.Domain/Analytics/Models/OpportunityConversionRatio.cs
@@ -4,9 +4,13 @@ namespace Yoma.Core.Domain.Analytics.Models
   {
     public int ViewedCount { get; set; }
 
+    public int ViewedCountFromNavigatedExternalLinkTracking { get; set; }
+
     public int NavigatedExternalLinkCount { get; set; }
 
     public int CompletedCount { get; set; }
+
+    public int CompletedCountFromNavigatedExternalLinkTracking { get; set; }
 
     public decimal ViewedToNavigatedExternalLinkPercentage { get; set; }
 
