c9ffd73bf5edc0dd7ea1cd526d1bc73f82d31a31
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-03-25 08:26:04 +0200

    Linting

--- src/api/src/domain/Yoma.Core.Domain/MyOpportunity/Models/MyOpportunitySearchResultsVerificationFilesAdmin.cs
+++ src/api/src/domain/Yoma.Core.Domain/MyOpportunity/Models/MyOpportunitySearchResultsVerificationFilesAdmin.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.Http;
 
 namespace Yoma.Core.Domain.MyOpportunity.Models
 {
-    public class MyOpportunitySearchResultsVerificationFilesAdmin
-    {
-        public string OpportunityTitle { get; set; }
+  public class MyOpportunitySearchResultsVerificationFilesAdmin
+  {
+    public string OpportunityTitle { get; set; }
 
-        public int? TotalCount { get; set; }
+    public int? TotalCount { get; set; }
 
-        public List<IFormFile>? Files { get; set; }
-    }
+    public List<IFormFile>? Files { get; set; }
+  }
 }
