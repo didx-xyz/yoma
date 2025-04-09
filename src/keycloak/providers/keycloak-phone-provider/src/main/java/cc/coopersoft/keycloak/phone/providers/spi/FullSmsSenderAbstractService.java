2885a42e0ff293e8c89d5e32442b58bc6670912d
Jason Dicker <110084728+jasondicker@users.noreply.github.com>
2025-04-09 13:01:28 +0200

    Feature/keycloak whatsapp (#1336)
    * keycloak: WhatsApp support for Twilio Configuration

--- src/keycloak/providers/keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/providers/spi/FullSmsSenderAbstractService.java
+++ src/keycloak/providers/keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/providers/spi/FullSmsSenderAbstractService.java
@@ -1,9 +1,5 @@
 package cc.coopersoft.keycloak.phone.providers.spi;
 
-import cc.coopersoft.keycloak.phone.Utils;
-import cc.coopersoft.keycloak.phone.providers.constants.TokenCodeType;
-import cc.coopersoft.keycloak.phone.providers.exception.MessageSendException;
-
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.Optional;
@@ -13,6 +9,10 @@ import org.jboss.logging.Logger;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
 import org.keycloak.theme.Theme;
+
+import cc.coopersoft.keycloak.phone.Utils;
+import cc.coopersoft.keycloak.phone.providers.constants.TokenCodeType;
+import cc.coopersoft.keycloak.phone.providers.exception.MessageSendException;
 
 public abstract class FullSmsSenderAbstractService implements MessageSenderService {
 
@@ -38,8 +38,8 @@ public abstract class FullSmsSenderAbstractService implements MessageSenderServi
     @Override
     public void sendSmsMessage(TokenCodeType type, String phoneNumber, String code, int expires, String kind)
             throws MessageSendException {
-        final String defaultMessage = String.format("[%s] - " + type.label + " code: %s, expires: %s minute ",
-                realmDisplay, code, expires / 60);
+        final String defaultMessage = String.format("%s: %s is your %s code. This code expires in %s minutes.",
+                realmDisplay.toUpperCase(), code, type.label, expires / 60);
         final String MESSAGE = localizeMessage(type, phoneNumber, code, expires).orElse(defaultMessage);
         sendMessage(phoneNumber, MESSAGE);
     }
@@ -53,37 +53,6 @@ public abstract class FullSmsSenderAbstractService implements MessageSenderServi
      * @param expires code expiration in seconds
      * @return The localized string, else empty.
      */
-    // private Optional<String> localizeMessage(TokenCodeType type, String phoneNumber, String code, int expires) {
-    //     if (this.session != null) {
-    //         try {
-    //             // Get login theme
-    //             final String loginThemeName = session.getContext().getRealm().getLoginTheme();
-    //             final Theme loginTheme = session.theme().getTheme(loginThemeName, Theme.Type.LOGIN);
-    //             // Try get locale from user associated with phone number (if any)
-    //             final Optional<UserModel> user = Utils.findUserByPhone(session, session.getContext().getRealm(),
-    //                     phoneNumber);
-    //             final Optional<String> userLocale = user.map(u -> u.getFirstAttribute(UserModel.LOCALE));
-    //             // Use locale from user or default to realm locale
-    //             final String localeName = userLocale.isPresent() ? userLocale.get()
-    //                     : session.getContext().getRealm().getDefaultLocale();
-    //             final Locale locale = Locale.forLanguageTag(localeName);
-    //             // Get message template from login theme bundle
-    //             final Properties messages = loginTheme.getMessages(locale);
-    //             final String messageTemplate = messages.getProperty("smsCodeMessage");
-    //             // Return nothing when template can't be found
-    //             if (messageTemplate == null || messageTemplate.isBlank()) {
-    //                 return Optional.empty();
-    //             }
-    //             // Format message
-    //             MessageFormat mf = new MessageFormat(messageTemplate, locale);
-    //             return Optional.of(mf.format(new Object[] { realmDisplay, type.label, code, expires / 60 }));
-    //         } catch (Exception ex) {
-    //             logger.error("Error while trying to localize message", ex);
-    //             return Optional.empty();
-    //         }
-    //     }
-    //     return Optional.empty();
-    // }
     private Optional<String> localizeMessage(TokenCodeType type, String phoneNumber, String code, int expires) {
         if (this.session != null) {
             try {
