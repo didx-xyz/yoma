3bf0a7e71433537d49a3a3f1f62187b0ed11c55c
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-04-23 08:08:56 +0200

    Fixed typo

--- src/api/src/domain/Yoma.Core.Domain/Opportunity/Services/OpportunityService.cs
+++ src/api/src/domain/Yoma.Core.Domain/Opportunity/Services/OpportunityService.cs
@@ -7,7 +7,7 @@ using Yoma.Core.Domain.BlobProvider;
 using Yoma.Core.Domain.Core;
 using Yoma.Core.Domain.Core.Exceptions;
 using Yoma.Core.Domain.Core.Extensions;
-using Yoma.Core.Domain.Core.Helpers;s
+using Yoma.Core.Domain.Core.Helpers;
 using Yoma.Core.Domain.Core.Interfaces;
 using Yoma.Core.Domain.Notification;
 using Yoma.Core.Domain.Notification.Interfaces;
