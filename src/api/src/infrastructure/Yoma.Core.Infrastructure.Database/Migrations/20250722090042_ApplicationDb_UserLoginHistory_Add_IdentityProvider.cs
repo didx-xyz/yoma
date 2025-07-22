ba0c2b30c7c904198f17a4a060e67dca72ebc98e
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-07-22 11:01:03 +0200

    UserLoginHistory: Added IdentityProvider

---
+++ src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20250722090042_ApplicationDb_UserLoginHistory_Add_IdentityProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yoma.Core.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationDb_UserLoginHistory_Add_IdentityProvider : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityProvider",
+                schema: "Entity",
+                table: "UserLoginHistory",
+                type: "varchar(100)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentityProvider",
+                schema: "Entity",
+                table: "UserLoginHistory");
+        }
+    }
+}
