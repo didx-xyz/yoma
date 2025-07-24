d6a6eff6e13513d0f60d0291588ff3f828259f9e
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-07-24 09:17:20 +0200

    Linting

--- src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20250724070028_ApplicationDb_UserLoginHistory_Indexing.cs
+++ src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20250724070028_ApplicationDb_UserLoginHistory_Indexing.cs
@@ -1,40 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Yoma.Core.Infrastructure.Database.Migrations
 {
+  /// <inheritdoc />
+  public partial class ApplicationDb_UserLoginHistory_Indexing : Migration
+  {
     /// <inheritdoc />
-    public partial class ApplicationDb_UserLoginHistory_Indexing : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_UserLoginHistory_UserId_ClientId_DateCreated",
-                schema: "Entity",
-                table: "UserLoginHistory");
+      migrationBuilder.DropIndex(
+          name: "IX_UserLoginHistory_UserId_ClientId_DateCreated",
+          schema: "Entity",
+          table: "UserLoginHistory");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_UserLoginHistory_UserId_ClientId_IdentityProvider_DateCreat~",
-                schema: "Entity",
-                table: "UserLoginHistory",
-                columns: new[] { "UserId", "ClientId", "IdentityProvider", "DateCreated" });
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_UserLoginHistory_UserId_ClientId_IdentityProvider_DateCreat~",
-                schema: "Entity",
-                table: "UserLoginHistory");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UserLoginHistory_UserId_ClientId_DateCreated",
-                schema: "Entity",
-                table: "UserLoginHistory",
-                columns: new[] { "UserId", "ClientId", "DateCreated" });
-        }
+      migrationBuilder.CreateIndex(
+          name: "IX_UserLoginHistory_UserId_ClientId_IdentityProvider_DateCreat~",
+          schema: "Entity",
+          table: "UserLoginHistory",
+          columns: new[] { "UserId", "ClientId", "IdentityProvider", "DateCreated" });
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropIndex(
+          name: "IX_UserLoginHistory_UserId_ClientId_IdentityProvider_DateCreat~",
+          schema: "Entity",
+          table: "UserLoginHistory");
+
+      migrationBuilder.CreateIndex(
+          name: "IX_UserLoginHistory_UserId_ClientId_DateCreated",
+          schema: "Entity",
+          table: "UserLoginHistory",
+          columns: new[] { "UserId", "ClientId", "DateCreated" });
+    }
+  }
 }
