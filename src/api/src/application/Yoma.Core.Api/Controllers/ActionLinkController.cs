aeb9f0ac692138fa3e5372970de49beefdfec217
Jason Dicker <dickerjason@gmail.com>
2025-07-30 06:38:22 +0200

    YOMA-817/ui-remove-approval-process

--- src/api/src/application/Yoma.Core.Api/Controllers/ActionLinkController.cs
+++ src/api/src/application/Yoma.Core.Api/Controllers/ActionLinkController.cs
@@ -10,123 +10,123 @@ using Yoma.Core.Domain.Core;
 namespace Yoma.Core.Api.Controllers
 {
 
-  [Route($"api/{Common.Constants.Api_Version}/actionLink")]
-  [ApiController]
-  [Authorize(Policy = Common.Constants.Authorization_Policy)]
-  [SwaggerTag("(by default, Admin or Organization Admin roles required)")]
-  public class ActionLinkController : Controller
-  {
-    #region Class Variables
-    private readonly ILogger<ActionLinkController> _logger;
-    private readonly ILinkService _linkService;
-    #endregion
+	[Route($"api/{Common.Constants.Api_Version}/actionLink")]
+	[ApiController]
+	[Authorize(Policy = Common.Constants.Authorization_Policy)]
+	[SwaggerTag("(by default, Admin or Organization Admin roles required)")]
+	public class ActionLinkController : Controller
+	{
+		#region Class Variables
+		private readonly ILogger<ActionLinkController> _logger;
+		private readonly ILinkService _linkService;
+		#endregion
 
-    #region Constructor
-    public ActionLinkController(
-        ILogger<ActionLinkController> logger,
-        ILinkService linkService)
-    {
-      _logger = logger;
-      _linkService = linkService;
-    }
-    #endregion
+		#region Constructor
+		public ActionLinkController(
+			ILogger<ActionLinkController> logger,
+			ILinkService linkService)
+		{
+			_logger = logger;
+			_linkService = linkService;
+		}
+		#endregion
 
-    #region Public Members
-    #region Anonymous Actions
-    [SwaggerOperation(Summary = "Get or create a sharing link for a published or expired entity by id (Anonymous)",
-     Description = "Optionally include a QR code")]
-    [HttpPost("create/sharing")]
-    [ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
-    [AllowAnonymous]
-    public async Task<IActionResult> GetOrCreateLinkSharing([FromBody] LinkRequestCreateShare request)
-    {
-      _logger.LogInformation("Handling request {requestName}", nameof(GetOrCreateLinkSharing));
+		#region Public Members
+		#region Anonymous Actions
+		[SwaggerOperation(Summary = "Get or create a sharing link for a published or expired entity by id (Anonymous)",
+		 Description = "Optionally include a QR code")]
+		[HttpPost("create/sharing")]
+		[ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
+		[AllowAnonymous]
+		public async Task<IActionResult> GetOrCreateLinkSharing([FromBody] LinkRequestCreateShare request)
+		{
+			_logger.LogInformation("Handling request {requestName}", nameof(GetOrCreateLinkSharing));
 
-      var result = await _linkService.GetOrCreateShare(request, true, false);
+			var result = await _linkService.GetOrCreateShare(request, true, false);
 
-      _logger.LogInformation("Request {requestName} handled", nameof(GetOrCreateLinkSharing));
+			_logger.LogInformation("Request {requestName} handled", nameof(GetOrCreateLinkSharing));
 
-      return StatusCode((int)HttpStatusCode.OK, result);
-    }
-    #endregion Anonymous Actions
+			return StatusCode((int)HttpStatusCode.OK, result);
+		}
+		#endregion Anonymous Actions
 
-    #region Administrative Actions
-    [SwaggerOperation(Summary = "Create an instant-verify link")]
-    [HttpPost("create/instantVerify")]
-    [ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
-    [Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
-    public async Task<IActionResult> CreateLinkInstantVerify([FromBody] LinkRequestCreateVerify request)
-    {
-      _logger.LogInformation("Handling request {requestName}", nameof(CreateLinkInstantVerify));
+		#region Administrative Actions
+		[SwaggerOperation(Summary = "Create an instant-verify link")]
+		[HttpPost("create/instantVerify")]
+		[ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
+		[Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
+		public async Task<IActionResult> CreateLinkInstantVerify([FromBody] LinkRequestCreateVerify request)
+		{
+			_logger.LogInformation("Handling request {requestName}", nameof(CreateLinkInstantVerify));
 
-      var result = await _linkService.CreateVerify(request, true);
+			var result = await _linkService.CreateVerify(request, true);
 
-      _logger.LogInformation("Request {requestName} handled", nameof(CreateLinkInstantVerify));
+			_logger.LogInformation("Request {requestName} handled", nameof(CreateLinkInstantVerify));
 
-      return StatusCode((int)HttpStatusCode.OK, result);
-    }
+			return StatusCode((int)HttpStatusCode.OK, result);
+		}
 
-    [SwaggerOperation(Summary = "Get the link by id",
-      Description = "Optionally include a QR code")]
-    [HttpGet("{linkId}")]
-    [ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
-    [Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
-    public IActionResult GetById([FromRoute] Guid linkId, [FromQuery] bool? includeQRCode)
-    {
-      _logger.LogInformation("Handling request {requestName}", nameof(GetById));
+		[SwaggerOperation(Summary = "Get the link by id",
+		  Description = "Optionally include a QR code")]
+		[HttpGet("{linkId}")]
+		[ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
+		[Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
+		public IActionResult GetById([FromRoute] Guid linkId, [FromQuery] bool? includeQRCode)
+		{
+			_logger.LogInformation("Handling request {requestName}", nameof(GetById));
 
-      var result = _linkService.GetById(linkId, true, includeQRCode);
-      _logger.LogInformation("Request {requestName} handled", nameof(GetById));
+			var result = _linkService.GetById(linkId, true, includeQRCode);
+			_logger.LogInformation("Request {requestName} handled", nameof(GetById));
 
-      return StatusCode((int)HttpStatusCode.OK, result);
-    }
+			return StatusCode((int)HttpStatusCode.OK, result);
+		}
 
-    [SwaggerOperation(Summary = "Search for links based on the supplied filter")]
-    [HttpPost("search")]
-    [ProducesResponseType(typeof(LinkSearchResult), (int)HttpStatusCode.OK)]
-    [Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
-    public IActionResult Search([FromBody] LinkSearchFilter filter)
-    {
-      _logger.LogInformation("Handling request {requestName}", nameof(Search));
+		[SwaggerOperation(Summary = "Search for links based on the supplied filter")]
+		[HttpPost("search")]
+		[ProducesResponseType(typeof(LinkSearchResult), (int)HttpStatusCode.OK)]
+		[Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
+		public IActionResult Search([FromBody] LinkSearchFilter filter)
+		{
+			_logger.LogInformation("Handling request {requestName}", nameof(Search));
 
-      var result = _linkService.Search(filter, true);
-      _logger.LogInformation("Request {requestName} handled", nameof(Search));
+			var result = _linkService.Search(filter, true);
+			_logger.LogInformation("Request {requestName} handled", nameof(Search));
 
-      return StatusCode((int)HttpStatusCode.OK, result);
-    }
+			return StatusCode((int)HttpStatusCode.OK, result);
+		}
 
-    [SwaggerOperation(Summary = "Update link status (Active / Deleted)",
-       Description = "Inactive links can be activated. Active or inactive links can be deleted")]
-    [HttpPatch("{linkId}/status")]
-    [ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
-    [Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
-    public async Task<IActionResult> UpdateStatus([FromRoute] Guid linkId, [FromRoute] LinkStatus status)
-    {
-      _logger.LogInformation("Handling request {requestName}", nameof(UpdateStatus));
+		[SwaggerOperation(Summary = "Update link status (Active / Deleted)",
+		   Description = "Inactive links can be activated. Active or inactive links can be deleted")]
+		[HttpPatch("{linkId}/status")]
+		[ProducesResponseType(typeof(LinkInfo), (int)HttpStatusCode.OK)]
+		[Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
+		public async Task<IActionResult> UpdateStatus([FromRoute] Guid linkId, [FromQuery] LinkStatus status)
+		{
+			_logger.LogInformation("Handling request {requestName}", nameof(UpdateStatus));
 
-      var result = await _linkService.UpdateStatus(linkId, status, true);
-      _logger.LogInformation("Request {requestName} handled", nameof(UpdateStatus));
+			var result = await _linkService.UpdateStatus(linkId, status, true);
+			_logger.LogInformation("Request {requestName} handled", nameof(UpdateStatus));
 
-      return StatusCode((int)HttpStatusCode.OK, result);
-    }
+			return StatusCode((int)HttpStatusCode.OK, result);
+		}
 
-    [SwaggerOperation(
-    Summary = "Search link usage based on the supplied filter",
-      Description = "Returns the link details and a paged list of usage entries. Unclaimed results are only available if the link has a distribution list")]
-    [HttpPost("search/usage")]
-    [ProducesResponseType(typeof(LinkSearchResultsUsage), (int)HttpStatusCode.OK)]
-    [Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
-    public IActionResult SearchUsage([FromBody] LinkSearchFilterUsage filter)
-    {
-      _logger.LogInformation("Handling request {requestName}", nameof(SearchUsage));
+		[SwaggerOperation(
+		Summary = "Search link usage based on the supplied filter",
+		  Description = "Returns the link details and a paged list of usage entries. Unclaimed results are only available if the link has a distribution list")]
+		[HttpPost("search/usage")]
+		[ProducesResponseType(typeof(LinkSearchResultsUsage), (int)HttpStatusCode.OK)]
+		[Authorize(Roles = $"{Constants.Role_Admin}, {Constants.Role_OrganizationAdmin}")]
+		public IActionResult SearchUsage([FromBody] LinkSearchFilterUsage filter)
+		{
+			_logger.LogInformation("Handling request {requestName}", nameof(SearchUsage));
 
-      var result = _linkService.SearchUsage(filter, true);
+			var result = _linkService.SearchUsage(filter, true);
 
-      _logger.LogInformation("Request {requestName} handled", nameof(SearchUsage));
+			_logger.LogInformation("Request {requestName} handled", nameof(SearchUsage));
 
-      return Ok(result);
-    }
-    #endregion Administrative Actions
-    #endregion
-  }
+			return Ok(result);
+		}
+		#endregion Administrative Actions
+		#endregion
+	}
 }
