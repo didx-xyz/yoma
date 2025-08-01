c6bb3c321486d7b90451454b1cf8561bebd0c268
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-08-01 10:27:38 +0200

    Linting

--- src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20250801055727_ApplicationDb_ActionLink_Log_UsernameClaimed.cs
+++ src/api/src/infrastructure/Yoma.Core.Infrastructure.Database/Migrations/20250801055727_ApplicationDb_ActionLink_Log_UsernameClaimed.cs
@@ -1,52 +1,52 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Yoma.Core.Infrastructure.Database.Migrations
 {
+  /// <inheritdoc />
+  public partial class ApplicationDb_ActionLink_Log_UsernameClaimed : Migration
+  {
     /// <inheritdoc />
-    public partial class ApplicationDb_ActionLink_Log_UsernameClaimed : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_UsageLog_DateCreated",
-                schema: "ActionLink",
-                table: "UsageLog");
+      migrationBuilder.DropIndex(
+          name: "IX_UsageLog_DateCreated",
+          schema: "ActionLink",
+          table: "UsageLog");
 
-            migrationBuilder.AddColumn<string>(
-                name: "UsernameClaimed",
-                schema: "ActionLink",
-                table: "UsageLog",
-                type: "varchar(320)",
-                nullable: true);
+      migrationBuilder.AddColumn<string>(
+          name: "UsernameClaimed",
+          schema: "ActionLink",
+          table: "UsageLog",
+          type: "varchar(320)",
+          nullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_UsageLog_UsernameClaimed_DateCreated",
-                schema: "ActionLink",
-                table: "UsageLog",
-                columns: new[] { "UsernameClaimed", "DateCreated" });
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_UsageLog_UsernameClaimed_DateCreated",
-                schema: "ActionLink",
-                table: "UsageLog");
-
-            migrationBuilder.DropColumn(
-                name: "UsernameClaimed",
-                schema: "ActionLink",
-                table: "UsageLog");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UsageLog_DateCreated",
-                schema: "ActionLink",
-                table: "UsageLog",
-                column: "DateCreated");
-        }
+      migrationBuilder.CreateIndex(
+          name: "IX_UsageLog_UsernameClaimed_DateCreated",
+          schema: "ActionLink",
+          table: "UsageLog",
+          columns: new[] { "UsernameClaimed", "DateCreated" });
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropIndex(
+          name: "IX_UsageLog_UsernameClaimed_DateCreated",
+          schema: "ActionLink",
+          table: "UsageLog");
+
+      migrationBuilder.DropColumn(
+          name: "UsernameClaimed",
+          schema: "ActionLink",
+          table: "UsageLog");
+
+      migrationBuilder.CreateIndex(
+          name: "IX_UsageLog_DateCreated",
+          schema: "ActionLink",
+          table: "UsageLog",
+          column: "DateCreated");
+    }
+  }
 }
