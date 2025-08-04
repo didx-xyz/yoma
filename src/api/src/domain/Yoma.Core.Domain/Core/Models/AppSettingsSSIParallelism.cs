707484b5b602faa2c203c6043f95eec94b9ea380
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-08-04 12:12:49 +0200

    Add appsettings configuration for SSI parallelism limits

---
+++ src/api/src/domain/Yoma.Core.Domain/Core/Models/AppSettingsSSIParallelism.cs
@@ -0,0 +1,9 @@
+namespace Yoma.Core.Domain.Core.Models
+{
+  public class AppSettingsSSIParallelism
+  {
+    public int TenantCreation { get; set; }
+
+    public int CredentialIssuance { get; set; }
+  }
+}
