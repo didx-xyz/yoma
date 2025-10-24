eda6916b6d17b317d7da9d11699e691ba538ad1a
adrianwium <82496337+adrianwium@users.noreply.github.com>
2025-10-24 08:06:58 +0200

    Renamed link/{id}/usage >> link/usage/{id}

--- src/api/src/application/Yoma.Core.Api/Controllers/ReferralController.cs
+++ src/api/src/application/Yoma.Core.Api/Controllers/ReferralController.cs
@@ -183,7 +183,7 @@ namespace Yoma.Core.Api.Controllers
 
     [SwaggerOperation(Summary = "Get usage for a link by usage ID (Authenticated User)",
       Description = "Admins can fetch any usage. Referrers can fetch usage for links they own. Referees can fetch usage for links they have claimed")]
-    [HttpGet("link/{id}/usage")]
+    [HttpGet("link/usage/{id}")]
     [Authorize(Roles = $"{Constants.Role_User}")]
     public ActionResult<ReferralLinkUsageInfo> GetUsageById([FromRoute] Guid id)
     {
