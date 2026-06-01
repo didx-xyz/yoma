5032b887f05dc4d31ef330e4f84241fd00a37be3
Adrian Wium <adrian.wium@didx.co.za>
2026-06-01 12:55:47 +0200

    Updated OpportunityUpsertOptions defaults

--- src/api/src/domain/Yoma.Core.Domain/Opportunity/Models/OpportunityUpsertOptions.cs
+++ src/api/src/domain/Yoma.Core.Domain/Opportunity/Models/OpportunityUpsertOptions.cs
@@ -2,13 +2,13 @@ using Yoma.Core.Domain.Core;
 
 namespace Yoma.Core.Domain.Opportunity.Models
 {
-  public class OpportunityUpsertOptions
+  public sealed class OpportunityUpsertOptions
   {
     public bool EnsureOrganizationAuthorization { get; set; }
 
-    public bool RaiseEvents { get; set; }
+    public bool RaiseEvents { get; set; } = true;
 
-    public bool SendNotifications { get; set; }
+    public bool SendNotifications { get; set; } = true;
 
     public SyncType? SyncTypeActionedBy { get; set; }
 
