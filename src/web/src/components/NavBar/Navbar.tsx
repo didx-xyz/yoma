6ed87f7f503a44315402a0844511840efbd6fef3
Jason Dicker <110084728+jasondicker@users.noreply.github.com>
2026-08-05 12:44:54 +0200

    feat: opportunity details - overall icon and button clean (#1912)

--- src/web/src/components/NavBar/Navbar.tsx
+++ src/web/src/components/NavBar/Navbar.tsx
@@ -583,6 +583,7 @@ export const Navbar: React.FC<{ theme: string }> = (theme) => {
                       <SignInButton
                         className="!btn-sm"
                         tabIndex={isDrawerOpen ? 0 : -1}
+                        showLabel={true}
                       />
                     )}
 
